f5772e3d110c9acce96353e17ec1210b6406a995
Javier Torrez <javierctorrez@gmail.com>
2017-06-04 18:21:51 -0400

    added comments, cleaned up code

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -4,7 +4,6 @@ import java.util.regex.Pattern;
 
 public class GameBoard {
 	private int boardSize; 
-	private String winningSymbol;
 	private String playerSymbol;
 	private String CPUSymbol;
 	private String[][] board;
@@ -12,13 +11,12 @@ public class GameBoard {
 	private Scanner scanner;
 	
 	public GameBoard(int size, Scanner scanner){
-	  this.scanner = scanner;
-	  this.boardSize = size * size;
-	  this.playerSymbol = getUserSymbol();
-	  this.CPUSymbol = playerSymbol.equals("X") ? "O" : "X";
-	  System.out.println("CPUSymbol is: " + CPUSymbol);
-	  this.board = new String[size][size];
-
+	  this.scanner = scanner; // assign Scanner object locally to properly close resource
+	  this.boardSize = size * size; // set capacity of board
+	  this.playerSymbol = getUserSymbol(); // assign symbol (X,O) to represent user moves
+	  this.CPUSymbol = playerSymbol.equals("X") ? "O" : "X"; // assign symbol (X,O) to represent CPU moves
+	  this.board = new String[size][size]; // initialize board array to hold moves and empty slots
+	  
 	  for(int i = 0, len = board.length; i < len; i++){
 		  for(int z = 0, len2 = board[i].length; z < len2; z++){
 			  board[i][z] = "-";
@@ -31,22 +29,12 @@ public class GameBoard {
 		this(size, new Scanner(System.in));
 	}
 	
-	private String getUserSymbol(){  
-	  System.out.println("Please pick your symbol (O or X):");
-	  String symbol = scanner.next();
-	  System.out.println("Your symbol is: " + symbol);
-	  return symbol;
-	}
-
-	public void displayBoard(){
-		for(int i = 0, len = board.length; i < len; i++){
-		  System.out.println();
-		    for(int z = 0, len2 = board[i].length; z < len2; z++){
-			  System.out.print(board[i][z] + " ");
-		    }
-		}
-	}
-	
+	/**	
+	 * Modify board by making a move for the respective player (CPU or human)
+	 * 
+	 * @param move			Move object representing the desired move
+	 * @param isPlayerTurn	Check who's making this move; Human or computer?
+	 */
 	public void makeMove(Move move, boolean isPlayerTurn){
 		int x = move.x - 1;
 		int y = move.y - 1;
@@ -93,54 +81,28 @@ public class GameBoard {
 
 			Move userMove = askAndValidateMove();
 			makeMove(userMove, true);
-			
 		}
 	}
 	
-	public Move askAndValidateMove(){
-		Move userMove;
-		
-		System.out.println("\nPick your move. [Pattern should be 'y,x']:");
-		String userInput = scanner.next();
-		String[] userInputArr = userInput.split(",");
-		boolean stringMatches = Pattern.matches("\\d{1},\\d{1}", userInput);
+	/* ----- Utility Methods ----- */
 
-		if(stringMatches){
-			userMove = new Move(userInputArr[0], userInputArr[1]);
-			return userMove;
-		} else {
-			System.out.println("Sorry, your input must match the pattern 'y,x'. Try again: ");
-			Move fixedUserMove = askAndValidateMove();
-			return fixedUserMove;
-		}
-	}
-		
-	public void testMove(Move move, boolean isPlayerTurn){
-		int x = move.x - 1;
-		int y = move.y - 1;
-		
-		if(isPlayerTurn){
-			board[x][y] = playerSymbol;
-		} else {
-			board[x][y] = CPUSymbol;
-		}
+	/*
+	 * Ask the user to pick which symbol identifies them, return it
+	 */
+	private String getUserSymbol(){  
+	  System.out.println("Please pick your symbol (O or X):");
+	  String symbol = scanner.next();
+	  System.out.println("Your symbol is: " + symbol);
+	  return symbol;
 	}
 	
-	public void undoMove(Move move){
-		int x = move.x - 1;
-		int y = move.y - 1;
-		
-		board[x][y] = "-";
-		possibleMoves.add(move);
-	}
-	
-	private boolean removeMove(Move toRemove){
-		return (possibleMoves.remove(toRemove));
-	}
-	
-	public BestMove chooseMove(boolean isPlayerTurn, Move lastMove){
+	/*
+	 * Minimax algorithm implementation used by the CPU to discover the best move to make 
+	 */
+	private BestMove chooseMove(boolean isPlayerTurn, Move lastMove){
 		BestMove best = new BestMove(1,1);
 		BestMove reply; 
+		String winningSymbol;
 		
 		if(hasWinner(lastMove, !isPlayerTurn)){
 			boolean lastPlayer = !isPlayerTurn;
@@ -176,10 +138,78 @@ public class GameBoard {
 				best.score = reply.score;
 			} 
 		}
-		
 		return best;
 	}
 	
+	/*
+	 * Method used by chooseMove to test 'theoretical' moves when building game tree
+	 */
+	private void testMove(Move move, boolean isPlayerTurn){
+		int x = move.x - 1;
+		int y = move.y - 1;
+		
+		if(isPlayerTurn){
+			board[x][y] = playerSymbol;
+		} else {
+			board[x][y] = CPUSymbol;
+		}
+	}
+	
+	/*
+	 * Displays the current state of the board
+	 */
+	private void displayBoard(){
+		for(int i = 0, len = board.length; i < len; i++){
+		  System.out.println();
+		    for(int z = 0, len2 = board[i].length; z < len2; z++){
+			  System.out.print(board[i][z] + " ");
+		    }
+		}
+	}
+	
+	/*
+	 * Asks user to write their move position, then validates if it was written correctly.
+	 * If valid, return Move object. If invalid, tell them to try again.
+	 */
+	private Move askAndValidateMove(){
+		Move userMove;
+		
+		System.out.println("\nPick your move. [Pattern should be 'y,x']:");
+		String userInput = scanner.next();
+		String[] userInputArr = userInput.split(",");
+		boolean stringMatches = Pattern.matches("\\d{1},\\d{1}", userInput);
+
+		if(stringMatches){
+			userMove = new Move(userInputArr[0], userInputArr[1]);
+			return userMove;
+		} else {
+			System.out.println("Sorry, your input must match the pattern 'y,x'. Try again: ");
+			Move fixedUserMove = askAndValidateMove();
+			return fixedUserMove;
+		}
+	}
+	
+	/*
+	 * Undoes a move after testing it on game board
+	 */
+	public void undoMove(Move move){
+		int x = move.x - 1;
+		int y = move.y - 1;
+		
+		board[x][y] = "-";
+		possibleMoves.add(move);
+	}
+	
+	/*
+	 * Removes a move from the list of possible moves after it has been used
+	 */
+	private boolean removeMove(Move toRemove){
+		return (possibleMoves.remove(toRemove));
+	}
+	
+	/*
+	 * Checks if there's currently a winner given the current move and player associated with move
+	 */
 	private boolean hasWinner(Move move, boolean checkPlayerWin){
 		String symbolToCheck = (checkPlayerWin == true ? playerSymbol : CPUSymbol);
 		int currentX = move.x - 1;
@@ -220,7 +250,10 @@ public class GameBoard {
 		}
 	}
 	
-	public boolean isFull(){
+	/*
+	 * Checks if the board is currently full 
+	 */
+	private boolean isFull(){
 		boolean fullBoard = (boardSize - possibleMoves.size() == boardSize);
 		return fullBoard;
 	}
