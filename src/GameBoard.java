aefaad926686a4cc3c439b45dec8ad4f76e3fe4c
Javier Torrez <javierctorrez@gmail.com>
2017-06-04 00:27:27 -0400

    added method to get and validate correct user input

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,17 +1,14 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class GameBoard {
 	private int boardSize; 
-	private int boardFill = 0;
 	private String winningSymbol;
 	private String playerSymbol;
 	private String CPUSymbol;
 	private String[][] board;
 	private ArrayList<Move> possibleMoves = new ArrayList<>();
-	private boolean COMPUTER = false;
-	private boolean PLAYER = false;
-
 	
 	public GameBoard(int size, String playerSymbol){
 	  this.boardSize = size * size;
@@ -58,10 +55,11 @@ public class GameBoard {
 				board[x][y] = playerSymbol;
 				removeMove(move);
 				if(hasWinner(move, true)){
-					System.out.println("THE PLAYER WON!");
+					System.out.println("You won!");
 					return;
 				}
 				displayBoard();	
+				System.out.println();
 				if(fullBoard){
 					System.out.println("The board is full, and there's no winner. It's a tie!");
 				}
@@ -75,27 +73,47 @@ public class GameBoard {
 			
 			
 		} else {
-			System.out.println("\n" + "Computer responded with move: " + move);
 			board[x][y] = CPUSymbol;
 			removeMove(move);
 			if(hasWinner(move, false)){
-				System.out.println("THE COMPUTER WON!");
+				System.out.println("hasWinner returned: " + (hasWinner(move, false)));
+				System.out.println("You lost.");
 				return;
 			}
+			System.out.println("The computer responded:");
 			displayBoard();
 			
 			if(fullBoard){
+				System.out.println();
 				System.out.println("The board is full, and there's no winner. It's a tie!");
 			} else{
-				System.out.println("\n" + "Pick your next move:");
-				String[] userMove = sc.next().split(",");
-				makeMove(new Move(userMove[0], userMove[1]), true);
+				Move userMove = askAndValidateMove();
+				makeMove(userMove, true);
 			}
 			
 			
 		}
 	}
 	
+	public Move askAndValidateMove(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Pick your move. [Pattern should be 'y,x']:");
+		String userInput = sc.next();
+		String[] userInputArr = userInput.split(",");
+		Move userMove;
+		
+		boolean stringMatches = Pattern.matches("\\d{1},\\d{1}", userInput);
+		
+		if(stringMatches){
+			userMove = new Move(userInputArr[0], userInputArr[1]);
+			return userMove;
+		} else {
+			System.out.println("Sorry, your input must match the pattern 'y,x'. Try again: ");
+			Move fixedUserMove = askAndValidateMove();
+			return fixedUserMove;
+		}
+	}
+		
 	public void testMove(Move move, boolean isPlayerTurn){
 		int x = move.x - 1;
 		int y = move.y - 1;
@@ -122,70 +140,46 @@ public class GameBoard {
 	public BestMove chooseMove(boolean isPlayerTurn, Move lastMove){
 		BestMove best = new BestMove(1,1);
 		BestMove reply; 
-		System.out.println("Before checking winner: " + isPlayerTurn);
 		
 		if(hasWinner(lastMove, !isPlayerTurn)){
 			boolean lastPlayer = !isPlayerTurn;
 			winningSymbol = lastPlayer ? playerSymbol : CPUSymbol;
 			int score = winningSymbol == playerSymbol ? -1 : 1;
-			System.out.println("There was a winner, so I'm returning: " + score);
 			best.score = score;
 			return best;
 		} else if (isFull()){
 			best.score = 0;
-			System.out.println("There was a tie, so I'm returning: 0");
 			return best;
 		}
-		
-		System.out.println("After checking winner: " + isPlayerTurn);
-		
+				
 		if(isPlayerTurn){
 			best.score = 2;
-			System.out.println();
-			System.out.println("This is a human level");
 		} else {
 			best.score = -2;
-			System.out.println();
-			System.out.println("This is a computer level");
 		}
 		
 		int index = 0;
 		int size = possibleMoves.size();
-		System.out.println("The current size of the array of moves is: " + size);
 		if(!possibleMoves.isEmpty()){
 			while(index < size){
-				System.out.println("------------NEW MOVE---------");
 				Move m = possibleMoves.get(0);
 				testMove(m, isPlayerTurn);
-				System.out.println();
-				System.out.println("Current index is: " + index + " out of " + size + " possible moves");
-				System.out.println("Displaying board-------------");
 				index++;
-				displayBoard();
 				removeMove(m);
-				System.out.println();
-				displayMoves();
 				reply = chooseMove(!isPlayerTurn, m);
-				System.out.println("REPLY MOVE: " + reply);
-				System.out.println("The current best score is " + best);
-				System.out.println("Is this a computer turn? " + !isPlayerTurn);
-				System.out.println("Is the reply larger than the current best? " + (reply.score > best.score));
 				undoMove(m);
 	
 				if((isPlayerTurn && reply.score < best.score) || (!isPlayerTurn && best.score < reply.score)){
 					best.x = m.x;
 					best.y = m.y;
 					best.score = reply.score;
-					System.out.println("Reply met criteria and is: " + reply + " and current best is: " + best);
 				} 
 			}
 		}
-		System.out.println("This is a decision node, so I'm returning: " + best);
 		return best;
 	}
 	
 	public boolean hasWinner(Move move, boolean checkPlayerWin){
-		System.out.println("This is what the isFull method returns: " + (isFull() ? "true" : "false"));
 		
 		String symbolToCheck = (checkPlayerWin == true ? playerSymbol : CPUSymbol);
 		int currentX = move.x - 1;
@@ -200,27 +194,27 @@ public class GameBoard {
 		for(int i = 0; i < board.length; i++){
 			
 			// check row
-			if(board[currentX][i].equals(symbolToCheck)){
+			if(board[currentX][i] == symbolToCheck){
 				row++;
 			}
 			
 			// check col
-			if(board[i][currentY].equals(symbolToCheck)){
+			if(board[i][currentY] == symbolToCheck){
 				col++;
 			}
 			
 			// check diagonal 
-			if(board[i][i].equals(symbolToCheck)){
+			if(board[i][i] == symbolToCheck){
 				diag++;
 			}
 			
 			// check reverse diagonal
-			if(board[i][(n - 1)-i].equals(symbolToCheck)){
+			if(board[i][(n - 1)-i] == symbolToCheck){
 				rdiag++;
 			}
 		}
-		
-		if(row == n || col == n || diag == n || rdiag ==n){
+
+		if(row == n || col == n || diag == n || rdiag == n){
 			return true;
 		} else {
 			return false;
@@ -229,16 +223,12 @@ public class GameBoard {
 	
 	public boolean isFull(){
 		boolean fullBoard = (boardSize - possibleMoves.size() == boardSize);
-		System.out.println("The current fill level is: " + fullBoard + " out of a total size of " + boardSize);
 		return fullBoard;
 	}
 	
 	public static void main(String[] args){
 		GameBoard testBoard = new GameBoard(3);
-		System.out.println("Pick your first move [format should be 'x,y']:");
-		Scanner sc = new Scanner(System.in);
-		String userMove = sc.next();
-		String[] move = userMove.split(",");
-		testBoard.makeMove(new Move(move[0], move[1]), true);
+		Move move = testBoard.askAndValidateMove();
+		testBoard.makeMove(move, true);
 	}
 }
